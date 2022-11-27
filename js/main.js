34dacc57b2a3faae6c796cf7f278d911fd3c6f06
justonemi <2663477382@qq.com>
2022-11-27 18:56:51 +0800

    Site updated: 2022-11-27 18:56:50

--- js/main.js
+++ js/main.js
@@ -1,65 +1,113 @@
-/* global REDEFINE */
+/**
+ * Sets up Justified Gallery.
+ */
+if (!!$.prototype.justifiedGallery) {
+  var options = {
+    rowHeight: 140,
+    margins: 4,
+    lastRow: "justify"
+  };
+  $(".article-gallery").justifiedGallery(options);
+}
 
-window.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function() {
 
-  REDEFINE.themeInfo = {
-    theme: `Redefine v${REDEFINE.theme_config.version}`,
-    author: 'EvanNotFound',
-    repository: 'https://github.com/EvanNotFound/hexo-theme-redefine'
-  }
+  /**
+   * Shows the responsive navigation menu on mobile.
+   */
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
 
-  REDEFINE.localStorageKey = 'REDEFINE-THEME-STATUS';
 
-  REDEFINE.styleStatus = {
-    isExpandPageWidth: false,
-    isDark: false,
-    fontSizeLevel: 0,
-    isOpenPageAside: true
-  }
+  /**
+   * Controls the different versions of  the menu in blog post articles 
+   * for Desktop, tablet and mobile.
+   */
+  if ($(".post").length) {
+    var menu = $("#menu");
+    var nav = $("#menu > #nav");
+    var menuIcon = $("#menu-icon, #menu-icon-tablet");
 
-  // print theme base info
-  REDEFINE.printThemeInfo = () => {
-    console.log(`\n %c ${REDEFINE.themeInfo.theme} %c ${REDEFINE.themeInfo.repository} \n`, `color: #fadfa3; background: #333; padding: 5px 0;`, `background: #fadfa3; padding: 5px 0;`);
-  }
+    /**
+     * Display the menu on hi-res laptops and desktops.
+     */
+    if ($(document).width() >= 1440) {
+      menu.show();
+      menuIcon.addClass("active");
+    }
 
-  // set styleStatus to localStorage
-  REDEFINE.setStyleStatus = () => {
-    localStorage.setItem(REDEFINE.localStorageKey, JSON.stringify(REDEFINE.styleStatus));
-  }
-
-  // get styleStatus from localStorage
-  REDEFINE.getStyleStatus = () => {
-    let temp = localStorage.getItem(REDEFINE.localStorageKey);
-    if (temp) {
-      temp = JSON.parse(temp);
-      for (let key in REDEFINE.styleStatus) {
-        REDEFINE.styleStatus[key] = temp[key];
+    /**
+     * Display the menu if the menu icon is clicked.
+     */
+    menuIcon.click(function() {
+      if (menu.is(":hidden")) {
+        menu.show();
+        menuIcon.addClass("active");
+      } else {
+        menu.hide();
+        menuIcon.removeClass("active");
       }
-      return temp;
-    } else {
-      return null;
+      return false;
+    });
+
+    /**
+     * Add a scroll listener to the menu to hide/show the navigation links.
+     */
+    if (menu.length) {
+      $(window).on("scroll", function() {
+        var topDistance = menu.offset().top;
+
+        // hide only the navigation links on desktop
+        if (!nav.is(":visible") && topDistance < 50) {
+          nav.show();
+        } else if (nav.is(":visible") && topDistance > 100) {
+          nav.hide();
+        }
+
+        // on tablet, hide the navigation icon as well and show a "scroll to top
+        // icon" instead
+        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+          $("#menu-icon-tablet").show();
+          $("#top-icon-tablet").hide();
+        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+          $("#menu-icon-tablet").hide();
+          $("#top-icon-tablet").show();
+        }
+      });
+    }
+
+    /**
+     * Show mobile navigation menu after scrolling upwards,
+     * hide it again after scrolling downwards.
+     */
+    if ($( "#footer-post").length) {
+      var lastScrollTop = 0;
+      $(window).on("scroll", function() {
+        var topDistance = $(window).scrollTop();
+
+        if (topDistance > lastScrollTop){
+          // downscroll -> show menu
+          $("#footer-post").hide();
+        } else {
+          // upscroll -> hide menu
+          $("#footer-post").show();
+        }
+        lastScrollTop = topDistance;
+
+        // close all submenu"s on scroll
+        $("#nav-footer").hide();
+        $("#toc-footer").hide();
+        $("#share-footer").hide();
+
+        // show a "navigation" icon when close to the top of the page, 
+        // otherwise show a "scroll to the top" icon
+        if (topDistance < 50) {
+          $("#actions-footer > #top").hide();
+        } else if (topDistance > 100) {
+          $("#actions-footer > #top").show();
+        }
+      });
     }
   }
-
-  REDEFINE.refresh = () => {
-    REDEFINE.initUtils();
-    REDEFINE.initHeaderShrink();
-    REDEFINE.initModeToggle();
-    REDEFINE.initBack2Top();
-
-    if (REDEFINE.theme_config.local_search.enable === true) {
-      REDEFINE.initLocalSearch();
-    }
-
-    if (REDEFINE.theme_config.code_copy.enable === true) {
-      REDEFINE.initCodeCopy();
-    }
-
-    if (REDEFINE.theme_config.lazyload.enable === true) {
-      REDEFINE.initLazyLoad();
-    }
-  }
-
-  REDEFINE.printThemeInfo();
-  REDEFINE.refresh();
 });
